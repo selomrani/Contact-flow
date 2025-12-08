3e5f3ba3d7ad23c3db4c7c23b357dfefc8e42d96
Soufyane <elomranisoufyan@gmail.com>
2025-12-08 19:03:50 +0100

    done use case diagram

--- Readme.md
+++ Readme.md
@@ -1,7 +1,10 @@
 # PROJECT OVERVIEW
 
-## DIAGRAMS
+## DIAGRAMS :
 
 ### Class diagram
 
 ![Class Diagram](public/assets/img/Connect%20flow%20class%20diagram.png)
+
+### Use case diagram 
+![Use Case Diagram](public/assets/img/Use%20case%20diagram.png)
--- public/index.php
+++ public/index.php
@@ -4,17 +4,46 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Connect Flow - Hero Section</title>
+    <title>Connect Flow</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
 
-    <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon">
+    <link rel="shortcut icon" href="public/assets/img/Gemini_Generated_Image_r99gq2r99gq2r99g.png" type="image/x-icon">
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
-
+<header>
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top navbar-glass">
+    <div class="container">
+        <a class="navbar-brand d-flex align-items-center" href="#">
+            Connect Flow
+        </a>
+        
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto align-items-center">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#features">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#about">About</a>
+                </li>
+                <li class="nav-item ms-lg-2">
+                    <a href="login.php" class="btn btn-nav-login">Login</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+</header>
 <body>
 
     <div id="heroCarousel" class="carousel slide hero-section" data-bs-ride="carousel">
@@ -29,7 +58,7 @@
             
             <div class="carousel-item active">
                 <div class="hero-overlay"></div> 
-                <img src="assets/img/AAAABcuF-ASdk8ggKxXR_kgnbvX1BAotwohyNYWx_jn7ixfCdiToeoYBnF_pXm8sLOqG-yy9Rgu4dUtjKdaiX9QlT4j-tUKRmBouW4zz.jpg" alt="First slide">
+                <img src="assets/img/slide1.png" alt="First slide">
                 <div class="carousel-caption d-none d-md-block">
                     <h1 class="display-3 fw-bold mb-3">Welcome to Connect Flow</h1>
                     <p class="lead mb-4">Streamline your communication effortlessly.</p>
