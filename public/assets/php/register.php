70e865b92f9e078069671a806ba76fdcc9569c26
Soufyan <elomranisoufyan@gmail.com>
2025-12-16 11:06:21 +0100

    converted header.php to a reusable component

--- public/assets/php/register.php
+++ public/assets/php/register.php
@@ -12,12 +12,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $password = mysqli_real_escape_string($connection, $_POST["password"]);
     $confirm  = mysqli_real_escape_string($connection, $_POST["confirm_password"]);
 
-    // $username = $_POST["username"];
-    // $email = $_POST["email"];
-    // $password = $_POST["password"];
-    // $confirm = $_POST["confirm_password"];
-    // $phone = $_POST["phone"];
-
     if ($password !== $confirm) {
         echo "<script>alert('Passwords do not match!');</script>";
     } else {
